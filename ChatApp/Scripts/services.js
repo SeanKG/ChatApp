5fa726d74bfeb3c052b916157a30094ba448b3ee
SeanKG <Seankgraves@gmail.com>
2014-05-25 16:14:56 -0400

    Add all files

---
+++ ChatApp/Scripts/services.js
@@ -0,0 +1,38 @@
+﻿'use strict';
+
+
+(function(){
+
+    // Demonstrate how to register services
+    // In this case it is a simple value service.
+    var app = angular.module('app.services', ["SignalR"]);
+
+    app.value('version', '0.1');
+
+    app.factory('ChatHub', ['$rootScope', 'Hub', function ($rootScope, Hub) {
+        var ChatHub = this;
+
+        ChatHub.count = 0;
+        ChatHub.messages = [];
+
+        var hub = new Hub('ChatHub', {
+            //Client methods
+            'hello': function (message) {
+                ChatHub.messages.push(message);
+                $rootScope.$apply();
+            },
+        },
+            //Server method stubs for ease of access
+            ['Hello']
+        );
+
+        ChatHub.Hello = function () {
+            hub.Hello("Hello"); //Calling a server method
+        };
+
+
+        return ChatHub;
+
+    }]);
+
+})();
---
+++ ChatApp/SignalR/ChatHub.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+
+namespace App.ChatApp.SignalR
+{
+    [CLSCompliant(false)]
+    public class ChatHub : Hub
+    {
+        public void Hello(string message)
+        {
+            Clients.All.hello(message);
+        }
+    }
+}
